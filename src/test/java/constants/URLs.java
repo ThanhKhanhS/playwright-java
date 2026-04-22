2830037843a6a2a10c7e9e167676e669c7cfa993
ThanhKhanhS <21522835@gm.uit.edu.vn>
2026-04-22 11:35:06 +0700

    Broken Links

--- src/test/java/constants/URLs.java
+++ src/test/java/constants/URLs.java
@@ -4,4 +4,7 @@ public class URLs {
 
     public static final String FORM =
             "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+
+    public static final String BROKEN =
+            "https://testautomationpractice.blogspot.com/";
 }
