7a1e731f7b917386a5c4ec3d96868ff91a813256
ThanhKhanhS <21522835@gm.uit.edu.vn>
2026-04-29 13:41:14 +0700

    Key Press

--- src/main/java/org/example/pages/ExpandTestingNavigationPage.java
+++ src/main/java/org/example/pages/ExpandTestingNavigationPage.java
@@ -6,14 +6,14 @@ import com.microsoft.playwright.options.LoadState;
 public class ExpandTestingNavigationPage {
     private final Page page;
 
-    private static final String Try_It_Out_Btn = "//h3[a[normalize-space()='%s']]/ancestor::div[contains(@class,'card')]//a[normalize-space()='Try it out']";
+    private static final String Link_By_Text = "//h3[a[normalize-space()='%s']]";
 
     public ExpandTestingNavigationPage(Page page){
         this.page = page;
     }
 
     public void navigateTo(String cardTitle){
-        page.locator(String.format(Try_It_Out_Btn, cardTitle)).click();
+        page.locator(String.format(Link_By_Text, cardTitle)).click();
         page.waitForLoadState();
     }
 }
